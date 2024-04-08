951f422de082e77eca5c0a00ee51d50bef729b81
Shemuel Chukwukadibia <shemmy80@gmail.com>
2024-04-08 04:22:17 +0100

    Upload new recipes

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '180da8c9-24a7-49f3-bd1e-d525713ad6e6';
+export const MODAL_CLOSE_SEC = 2.5;
